73c111904230c1100bc9945bb61ad6b36f4ab056
ShreyaNK409 <78596107+ShreyaNK409@users.noreply.github.com>
2024-08-02 18:45:08 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,6 @@
+no=input("Enter a number")
+
+if no>20:
+    print("number is greater than 20")
+else:
+    print("number is lesser than 20")    
---
+++ homework.py
@@ -0,0 +1,7 @@
+x=input("Enter a number: ")
+y=input("Enter another number: ")
+
+ad=int(x)+int(y)
+
+
+print(ad)
